9be7f1f9b5047837c04aa544c6d98543207df3cc
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2018-06-13 02:27:13 +0200

    Multi lines indent function ready for Pull Request

--- src/main.py
+++ src/main.py
@@ -384,6 +384,12 @@ class Main(QMainWindow):
 
     def indenting(self):
         print("indenting:start")
+        # TODO: allow multiples lines to work on a indent on the whole document
+        # TODO: find why the blank on the last line would not allow to indent multiples lines
+        # ======================
+        # DONE: link the multi lines indent to the key binding
+        # DONE: place the cursor at the end of the indented part
+        # DONE: allow undo
         textBlock = self.editor.toPlainText()
 
         try:
