c3ac0cdc88d0e5ab2013bf2f76e600a994cea942
Orochi <jordansavell1198@gmail.com>
2018-07-06 00:02:06 +0100

    Fixed installation file

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-
 #!/bin/bash
 
-INSTALL_DIR="$HOME/pypad/"
+INSTALL_DIR=""
 PYPAD_DIR="/usr/bin/pypad"
 MAIN_DIR="/opt/pypad"
 
 function main() {
+        read -p "Enter home directory full path: " INSTALL_DIR
         echo "Creating filestructure for pypad"
         echo " "
         echo "Installing"
         run_structure >> /var/log/pypad.log
-        echo " "
         echo "All done. Cleaning up. Remove install.sh and directory manually for now."
+        create_files
         cleanup
+        exit
 }
 
 function run_structure() {
-
         sudo touch /opt/pypad/
         sudo cp $INSTALL_DIR/* /opt/pypad
 }
@@ -24,12 +24,11 @@ function run_structure() {
 function create_files() {
         sudo touch $PYPAD_DIR
         sudo chmod +x $PYPAD_DIR
-        echo "python3 $HOME/pypad/main.py" >> $PYPAD_DIR
+        echo "python3 $MAIN_DIR/src/main.py" > $PYPAD_DIR
 }
 
 function cleanup() {
-        sudo rm -rf $I
-        cd $HOME
+        sudo rm -rf $INSTALL_DIR/pypad/
 }
 
 main
