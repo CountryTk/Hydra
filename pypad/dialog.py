2d74705a4beb447d872822e2afe9791686af16a1
Jacob Culley <jacob@culley.me>
2018-07-10 20:00:56 +0100

    add quit dialog

--- pypad/dialog.py
+++ pypad/dialog.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QDialog, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QLabel, QPushButton, QVBoxLayout, QGridLayout
 
 dialogs = []
 
@@ -30,4 +30,33 @@ class Error(QDialog):
 class FatalError(Error):
 
     def closeEvent(self, event):
+        QCoreApplication.instance().quit()
+
+
+class Quit(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        dialogs.append(self)
+
+        layout = QGridLayout(self)
+
+        label = QLabel("Are you sure you want to quit?")
+        layout.addWidget(label, 0, 0, 1, 2)
+
+        yes = QPushButton('Yes')
+        yes.clicked.connect(self.quit)
+        layout.addWidget(yes, 1, 0)
+
+        no = QPushButton('No')
+        no.clicked.connect(self.closeEvent)
+        layout.addWidget(no, 1, 1)
+
+        self.setWindowTitle("Quit")
+        self.show()
+
+    def closeEvent(self, event):
+        dialogs.remove(self)
+
+    def quit(self):
         QCoreApplication.instance().quit()
