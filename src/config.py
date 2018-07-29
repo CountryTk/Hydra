b8f495b996fcc59461e1a52bebe89412b0a16996
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-07-29 20:10:01 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,23 @@
 import json
 
 
-def read():
-    try:
-        jsonFile = open("../config.json", "r")
-    except FileNotFoundError:
+def read(theme):
+    if theme == 0:
+        try:
+            jsonFile = open("../config.json", "r")
+
+        except FileNotFoundError:
+            jsonFile = open("../config.json", "r")
+
+    elif theme == 1:
+        jsonFile = open("../config1.json", "r")
+
+    elif theme == 2:
         jsonFile = open("../config.json", "r")
 
+    else:
+        print("Error occured")
+
     text = jsonFile.read()
-    jsonFile.close()
 
     return json.loads(text)
