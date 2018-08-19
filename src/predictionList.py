f131fd13bdd1a6447ec71ca1f4f463bc568eabc9
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-08-19 18:08:16 +0300

    Add files via upload

---
+++ src/predictionList.py
@@ -0,0 +1,13 @@
+list = ["abs()", "all()", "any()", "ascii()", "bin()", "bool()", "breakpoint()", "bytearray()", "bytes()", "callable()", "chr()", "classmethod()",
+"compile()", "complex()",
+"delattr()", "dict()", "dir()", "divmod()", "enumerate()", "eval()", "exec()", "filter()", "float()", "format()", "frozenset()",
+ "getattr()", "globals()", "hasattr()", "hash()",
+"help()", "hex()", "id()", "input()", "int()", "isinstance()", "issubclass()", "iter()", "len()", "list()", "locals()", "map()",
+"max()", "memoryview()", "min()", "next()", "object()",
+"oct()", "open()", "ord()", "pow()", "print()", "property()", "range()", "repr()", "reversed()", "round()", "set()", "setattr()",
+"slice()", "sorted()", "staticmethod()", "str()", "sum()",
+"super()", "tuple()", "type()", "vars()", "zip()", "__import__()", 'False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class',
+'continue', 'def', 'del', 'elif','else', 'except',
+'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda','nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try',
+'while', 'with', 'yield', 'endswith()']
+list = sorted(list)
