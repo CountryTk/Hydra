b25ec43b98316448442c5fc10f9bab511acb5645
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-08-21 21:59:09 +0300

    Update TerminalBarWidget.py

--- src/TerminalBarWidget.py
+++ src/TerminalBarWidget.py
@@ -1,12 +1,13 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QLineEdit
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtCore import QRect, Qt, pyqtSignal
 
 import config
 
 config0 = config.read(0)
 config1 = config.read(1)
 config2 = config.read(2)
+
 with open("default.json") as choice:
     choiceIndex = int(choice.read())
     print(str(choiceIndex))
@@ -14,14 +15,20 @@ lineBarColor = QColor(53, 53, 53)
 
 
 class TerminalBar(QWidget):
+
+    commandSignal = pyqtSignal(str)
+
     def __init__(self, parent=None, index=choiceIndex):
 
         super().__init__(parent)
-        self.editor = parent
-        self.editor.blockCountChanged.connect(self.update_width)
-        self.editor.updateRequest.connect(self.update_on_scroll)
-        self.update_width('1')
-        self.index = index
+        if parent is None:
+            pass
+        else:
+            self.editor = parent
+            self.editor.blockCountChanged.connect(self.update_width)
+            self.editor.updateRequest.connect(self.update_on_scroll)
+            self.update_width('1')
+            self.index = index
 
     def update_on_scroll(self, rect, scroll):
         if self.isVisible():
@@ -52,8 +59,9 @@ class TerminalBar(QWidget):
         if self.isVisible():
             block = self.editor.firstVisibleBlock()
             height = self.fontMetrics().height()
-            number = ">>>"
+            number = block.blockNumber()
             painter = QPainter(self)
+            symbol = ">>>"
             painter.fillRect(event.rect(), lineBarColor)
             if config['editor']['NumberBarBox'] is True:
                 painter.drawRect(0, 0, event.rect().width() - 1, event.rect().height() - 1)
@@ -61,21 +69,22 @@ class TerminalBar(QWidget):
             font = painter.font()
 
             current_block = self.editor.textCursor().block().blockNumber() + 1
-
             while block.isValid():
                 block_geometry = self.editor.blockBoundingGeometry(block)
                 offset = self.editor.contentOffset()
                 block_top = block_geometry.translated(offset).top()
-
+                number += 1
                 rect = QRect(0, block_top, self.width() - 5, height)
 
                 if number == current_block:
                     font.setBold(True)
+
                 else:
                     font.setBold(False)
 
                 painter.setFont(font)
-                painter.drawText(rect, Qt.AlignRight, number)
+                #painter.drawText(rect, Qt.AlignRight, number)
+                painter.drawText(rect, Qt.AlignRight, symbol)
 
                 if block_top > event.rect().bottom():
                     break
