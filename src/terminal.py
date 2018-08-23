6b31efd62da622f6a42f48ab76c7775e03bbb3df
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-08-23 13:52:19 +0300

    Add files via upload

---
+++ src/terminal.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class terminal(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.process = QProcess(self)
+
+        self.terminal = QWidget()
+        print(self.terminal.width())
+        self.terminal.setMinimumWidth(782)
+        self.terminal.setMinimumHeight(160)
+        # Works also with urxvt:
+        self.layout = QGridLayout()
+        self.process.start(
+                'urxvt',['-embed', str(int(self.terminal.winId()))])
+        self.layout.setColumnStretch(1, 5)
+        self.layout.addWidget(self.terminal, 1, 1)
+        self.setLayout(self.layout)
+
