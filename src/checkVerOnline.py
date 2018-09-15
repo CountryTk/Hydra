cc9323a19efdb7453faff2d37f22ab809d3e15ae
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-15 19:20:29 +0300

    Update checkVerOnline.py

--- src/checkVerOnline.py
+++ src/checkVerOnline.py
@@ -6,6 +6,6 @@ def checkVerOnlineFunc():
     url = urllib.request.urlopen('https://github.com/Fuchsiaff/PyPad/blob/master/src/version.txt').read()
 
     object = BeautifulSoup(url, 'lxml')
-    lol = object.find("td", class_="blob-code blob-code-inner js-file-line")
+    num = object.find("td", class_="blob-code blob-code-inner js-file-line")
 
-    return str(lol.text)
+    return str(num.text)
