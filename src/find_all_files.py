8d912466410f771c3e1c4b98ee4ec8e634ada90e
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-15 17:08:18 +0300

    Add document search

--- src/find_all_files.py
+++ src/find_all_files.py
@@ -1,41 +1,88 @@
 import os
 import sys
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLineEdit, QLabel, QPushButton
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLineEdit, QLabel, QVBoxLayout, QPushButton, QPlainTextEdit
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QThread, pyqtSignal
 
 class documentSearch(QWidget):
     def __init__(self):
         super().__init__()
         
+        self.font = QFont()
+        self.font.setFamily("Iosevka")
+        self.font.setPointSize(12)
+        
         self.setWindowTitle("Find a document")
         self.initUI()
         
     def initUI(self):
-        self.label = QLabel( "Find a document")
-        self.search_document_field = QLineEdit(self)
+        self.layout = QHBoxLayout(self)
+        self.vbox = QVBoxLayout()
+        self.label = QLabel("Enter a file name to search:")
+        
+        self.searchButton = QPushButton("Search")
+        self.cancelButton = QPushButton("Cancel")
+        self.searchResults = QPlainTextEdit()
+        self.infoLabel = QLabel("Results will be displayed below")
+        self.searchResults.setReadOnly(True)
+        
+        self.searchDocumentField = QLineEdit()
+        self.searchButton.clicked.connect(self.trigger)
+        self.cancelButton.clicked.connect(lambda: self.hide())
+        
+        self.searchButton.setAutoDefault(True)
+        
+        self.label.setFont(self.font)
+        self.infoLabel.setFont(self.font)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.searchDocumentField)
+        self.layout.addWidget(self.searchButton)
+        self.layout.addWidget(self.cancelButton)
+        self.vbox.addWidget(self.infoLabel)
+        self.vbox.addWidget(self.searchResults)
+        self.layout.addLayout(self.vbox)
+        self.setLayout(self.layout)
+        
+    def trigger(self):
+        
+        file = self.searchDocumentField.text()
+        self.thread = grabFiles(file)
+        self.thread.start()
+        self.thread.fileSignal.connect(self.showPath)
     
+    def showPath(self, path):
+        
+        self.searchResults.appendPlainText(path)
+        
     def run(self):
         
         self.show()
         
-    def find_all_files(self, file):
+class grabFiles(QThread):
+    fileSignal = pyqtSignal(str)
+    
+    def __init__(self, file):
+        super().__init__()
+        
+        self.file = file
+        
+    def run(self):
+        file = self.file
         operating_sys = sys.platform
         if operating_sys == "win32":
             thing = "\\"
         elif operating_sys == "linux":
             thing = '/'   
-    # print(os.getcwd())
-    # print(os.listdir())
-   #  print(sys.platform)
-    # os.chdir("../")
-    # print(os.getcwd())
-  
+            
         for i in os.walk('/'):
         
             for j in i:
                 if file in j or j == file:
-                    print("File found!")
-                    return "Path to file: " + str(i[0]) + thing + str(file)
-                 # print("File name: " + str(file))
+                    path = str(i[0] + thing + str(file))
+                    self.fileSignal.emit(path)
+                   # return "Path to file: " + str(i[0]) + thing + str(file)
+                
+                    
                 else:
                     pass
     
