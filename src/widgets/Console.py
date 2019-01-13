8b22b98da61e8c16a985b4363e973037650a5bbb
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2019-01-13 11:53:21 +0200

    Add a hide button

--- src/widgets/Console.py
+++ src/widgets/Console.py
@@ -1,5 +1,5 @@
 import os
-from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout
 from PyQt5.QtGui import QPainter, QColor, QSyntaxHighlighter, QTextCharFormat, QColor, QFont, QIcon
 from PyQt5.QtCore import QRect, Qt, pyqtSignal, QRegExp, QProcess
 from widgets.Editor import Editor
@@ -12,37 +12,41 @@ class Console(QWidget):
     errorSignal = pyqtSignal(str)
     outputSignal = pyqtSignal(str)
 
-    def __init__(self):
+    def __init__(self, parent=None):
         super().__init__()
-        self.editor = Editor(self)
-        self.editor.setReadOnly(True)
+        self.parent = parent
         self.custom = Customize()
+        self.pressed = False
         self.font = QFont()
-        self.numbers = NumberBar(self.editor, index=self.custom.index)
-
-        self.dialog = MessageBox()
+        self.dialog = MessageBox(self)
         # self.font.setFamily(editor["editorFont"])
         self.font.setFamily("Iosevka")
-        self.terminateButton = QPushButton()
-        self.terminateButton.setIcon(QIcon("resources/square.png"))
-        self.terminateButton.clicked.connect(self.terminate)
         self.font.setPointSize(12)
-        self.layout = QHBoxLayout()
-        self.layout.addWidget(self.numbers)
-        self.layout.addWidget(self.editor, 1)
-        self.layout.addWidget(self.terminateButton)
+        self.layout = QVBoxLayout()
 
         self.setLayout(self.layout)
         self.output = None
         self.setFocusPolicy(Qt.StrongFocus)
         self.error = None
         self.finished = False
-        self.editor.setFont(self.font)
 
         self.process = QProcess()
         self.state = None
         self.process.readyReadStandardError.connect(self.onReadyReadStandardError)
         self.process.readyReadStandardOutput.connect(self.onReadyReadStandardOutput)
+
+    def ispressed(self):
+        return self.pressed
+
+    def added(self):
+        self.pressed = True
+
+    def remove(self):
+        self.terminate()
+        self.editor.deleteLater()
+        self.button.deleteLater()
+        self.terminateButton.deleteLater()
+        self.pressed = False
 
     def onReadyReadStandardError(self):
         try:
@@ -56,30 +60,48 @@ class Console(QWidget):
             else:
                 self.error = self.error.split(os.linesep)[-2]
                 self.dialog.helpword = str(self.error)
-                self.dialog.getHelp()
+                self.dialog.getHelp(self.parent.parent)
         except IndexError as E:
-            print(E)
+            print(E, " on line 70 in the file Console.py")
 
     def onReadyReadStandardOutput(self):
         try:
             self.result = self.process.readAllStandardOutput().data().decode()
         except UnicodeDecodeError as E:
-            print(E)
-        self.editor.appendPlainText(self.result.strip("\n"))
-        self.state = self.process.state()
+            print(E, " on line 76 in the file Console.py")
+        try:
+            self.editor.appendPlainText(self.result.strip("\n"))
+            self.state = self.process.state()
+        except RuntimeError:
+            pass
 
         self.outputSignal.emit(self.result)
 
     def ifFinished(self, exitCode, exitStatus):
         self.finished = True
 
-    def run(self, command):
+    def add(self, command):
         """Executes a system command."""
         # clear previous text
-        self.editor.clear()
-        # self.editor.setPlainText("[" + str(getpass.getuser()) + "@" + str( socket.gethostname()) + "]" +
-                                 #"   ~/" + str(os.path.basename(os.getcwd())) + " >$")
+        self.added()
+        self.button = QPushButton("Hide terminal")
+        self.terminateButton = QPushButton("   Stop")
+        self.terminateButton.setIcon(QIcon("resources/square.png"))
+        self.terminateButton.clicked.connect(self.terminate)
+        self.button.setFixedWidth(100)
+        self.h_layout = QHBoxLayout()
+        self.editor = Editor(self)
+        self.editor.setReadOnly(True)
+        self.editor.setFont(self.font)
+        self.layout.addWidget(self.button)
+        self.layout.addLayout(self.h_layout)
+        self.h_layout.addWidget(self.editor)
+        self.h_layout.addWidget(self.terminateButton)
+        self.button.clicked.connect(self.remove)
+        self.run(command)
 
+    def run(self, command):
+        self.editor.clear()
         if self.process.state() == 1 or self.process.state() == 2:
             self.process.kill()
             self.editor.setPlainText("Process already started, terminating")
@@ -87,6 +109,5 @@ class Console(QWidget):
             self.process.start(command)
 
     def terminate(self):
-
         if self.process.state() == 2:
             self.process.kill()
