6d8ddea14a1811a2838100eeb9d4fb8ea75e6bbe
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2019-01-13 11:54:56 +0200

    Add clickable functions

--- src/widgets/Editor.py
+++ src/widgets/Editor.py
@@ -1,7 +1,7 @@
-from builtins import print, property
-from PyQt5.QtWidgets import QPlainTextEdit, QAction, QMenu, QInputDialog, QTextEdit, QWidget
-from PyQt5.QtCore import Qt, QRect, QSize, QObject, pyqtSignal
-from PyQt5.QtGui import QFont, QTextOption, QTextCursor, QTextFormat, QPainter, QColor
+from builtins import print
+from PyQt5.QtWidgets import QPlainTextEdit, QAction, QMenu, QInputDialog, QTextEdit, QWidget, QToolTip, QApplication
+from PyQt5.QtCore import Qt, QRect, QSize, QObject, pyqtSignal, QPoint
+from PyQt5.QtGui import QFont, QTextOption, QTextCursor, QTextFormat, QPainter, QColor, QCursor, QTextCharFormat
 from utils.find_all import find_all
 from widgets.Messagebox import MessageBox
 from utils.config import config_choice
@@ -28,20 +28,19 @@ class Editor(QPlainTextEdit):
         self.lineNumberArea = QLineNumberArea(self)
         self.blockCountChanged.connect(self.updateLineNumberAreaWidth)
         self.updateRequest.connect(self.updateLineNumberArea)
-        self.textChanged.connect(self.check)
         self.cursorPositionChanged.connect(self.highlightCurrentLine)
         self.updateLineNumberAreaWidth(0)
         self.parent = parent
         self.font = QFont()
         self.size = 12
-        self.dialog = MessageBox()
+        self.dialog = MessageBox(self)
         self.menu_font = QFont()
         self.menu_font.setFamily("Iosevka")
         self.menu_font.setPointSize(10)
         self.font.setFamily(editor["editorFont"])
         self.font.setPointSize(editor["editorFontSize"])
         self.focused = None
-
+        self.text = None
         self.replace_tabs = 4
         self.setWordWrapMode(4)
         self.setFont(self.font)
@@ -53,17 +52,47 @@ class Editor(QPlainTextEdit):
         self.createStandardContextMenu()
         self.setWordWrapMode(QTextOption.NoWrap)
 
+    def getTextCursor(self):
+        textCursor = self.textCursor()
+        textCursorPos = textCursor.position()
+
+        return textCursor, textCursorPos
+
     def get_linenumbers(self):
         return self.blockCount()
 
     def check(self):
         cursor = self.textCursor()
         b = cursor.block()
-        if len(b.text()) >= 79:
-            print("pep 8 violation on line: " + str(b.blockNumber() + 1))
+        extraSelections = []
+        if len(b.text()) > 120:
+            selection = QTextEdit.ExtraSelection()
+            # TODO: implement something using flake8
+            selection.format.setFontUnderline(True)
+            selection.format.setUnderlineColor(QColor("#FF0000"))
+
+            selection.format.setUnderlineStyle(QTextCharFormat.SpellCheckUnderline)
+            selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+
+            selection.cursor = self.textCursor()
+            selection.cursor.clearSelection()
+
+            extraSelections.append(selection)
+            self.setExtraSelections(extraSelections)
+
+            font = QFont()
+            font.setFamily("Iosevka")
+            font.setPointSize(10)
+
+            QToolTip.setFont(font)
+            cursor = self.textCursor()
+            current_line = cursor.positionInBlock()
+            QToolTip.showText(QCursor.pos(), "Line too long (" + str(current_line) + "> 120) | violation on line: " +
+                              str(b.blockNumber() + 1))
 
     def newFile(self):
-        """This and most of the functions below will just be wrappers for the functions defined in Main"""
+        """This is a wrapper for the function defined in Main"""
+
         self.new_action = QAction("New")
         self.new_action.triggered.connect(self.parent.parent.newFile)
 
@@ -103,6 +132,7 @@ class Editor(QPlainTextEdit):
             selection.cursor.clearSelection()
             extraSelections.append(selection)
         self.setExtraSelections(extraSelections)
+        self.check()
 
     def lineNumberAreaPaintEvent(self, event):
         painter = QPainter(self.lineNumberArea)
@@ -138,6 +168,12 @@ class Editor(QPlainTextEdit):
         self.run_action = QAction("Run")
         self.run_action.triggered.connect(self.parent.parent.terminal)
 
+    def textUnderCursor(self):
+        textCursor = self.textCursor()
+        textCursor.select(QTextCursor.WordUnderCursor)
+
+        return textCursor.selectedText()
+
     def contextMenuEvent(self, event):
 
         menu = QMenu()
@@ -145,7 +181,6 @@ class Editor(QPlainTextEdit):
         self.newFile()
         self.openFile()
         self.runFile()
-        print("aa")
         menu.addAction(self.new_action)
         menu.addAction(self.open_action)
         menu.addAction(self.run_action)
@@ -161,6 +196,48 @@ class Editor(QPlainTextEdit):
 
         textCursor.setPosition(textCursorPos - 1)
         self.setTextCursor(textCursor)
+
+    def mousePressEvent(self, e):
+
+        self.check()
+        if QApplication.queryKeyboardModifiers() == Qt.ControlModifier:
+            if self.text is not None:
+                self.check_func("lol")
+                url = "https://docs.python.org/3/library/functions.html#" + self.text
+                word = self.text
+                self.parent.parent.showBrowser(url, word)
+        QApplication.restoreOverrideCursor()
+        super().mousePressEvent(e)
+
+    def check_func(self, word):
+        funcs = [
+            "abs", "all", "any", "ascii", "bin", "bool", "breakpoint", "bytearray", "bytes", "callable",
+            "chr", "classmethod",
+            "compile", "complex",
+            "delattr", "dict", "dir", "divmod", "enumerate", "eval", "exec", "filter", "float",
+            "format",
+            "frozenset",
+            "getattr", "globals", "hasattr", "hash",
+            "help", "hex", "id", "input", "int", "isinstance", "issubclass", "iter", "len", "list",
+            "locals", "map",
+            "max", "memoryview", "min", "next", "object",
+            "oct", "open", "ord", "pow", "print", "property", "range", "repr", "reversed", "round",
+            "set", "setattr", "slice", "sorted", "staticmethod", "str", "sum",
+            "super", "tuple", "type", "vars", "zip", "__import__"
+        ]
+        word_array = list(word)
+        for wo in word_array:
+            if wo in ["{", "}", "'", '"', "[", "]", "(", ")"]:
+                word_array.remove(wo)
+        for w in funcs:
+            if w == "".join(word_array):
+                print("".join(word_array))
+                return True
+
+    def mouseReleaseEvent(self, e):
+
+        self.check()
+        super().mouseReleaseEvent(e)
 
     def keyPressEvent(self, e):
         textCursor = self.textCursor()
@@ -184,7 +261,7 @@ class Editor(QPlainTextEdit):
                 textCursorPos = textCursor.position()
 
             except (AttributeError, UnboundLocalError) as E:
-                print(E)
+                print(E, " on line 228 in the file Editor.py")
 
             if currentWidget is not None:
                 text, okPressed = QInputDialog.getText(self, 'Find', 'Find what: ')
@@ -201,11 +278,30 @@ class Editor(QPlainTextEdit):
                                 self.dialog.noMatch(text)
 
                     except FileNotFoundError as E:
-                        print(E)
+                        print(E, " on line 245 in the file Editor.py")
 
         if key == Qt.Key_QuoteDbl:
             self.insertPlainText('"')
             self.moveCursorPosBack()
+
+        if key == 16777249:
+            if self.check_func(self.textUnderCursor()):
+                extraSelections = []
+                selection = QTextEdit.ExtraSelection()
+                selection.format.setFontUnderline(True)
+                selection.format.setUnderlineColor(QColor("#00d2ff"))
+                selection.format.setBackground(QColor("#00d2ff"))
+                selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+
+                selection.cursor = self.textCursor()
+                selection.cursor.clearSelection()
+                selection.cursor.select(QTextCursor.WordUnderCursor)
+                extraSelections.append(selection)
+                self.setExtraSelections(extraSelections)
+                self.text = self.textUnderCursor()
+                QApplication.setOverrideCursor(Qt.PointingHandCursor)
+            else:
+                self.text = None
 
         if e.modifiers() == Qt.ControlModifier and e.key() == 61:  # Press Ctrl+Equal key to make font bigger
 
@@ -213,6 +309,7 @@ class Editor(QPlainTextEdit):
             self.font.setFamily(editor["editorFont"])
             self.setFont(self.font)
             self.size += 1
+
         if e.modifiers() == Qt.ControlModifier and e.key() == 16777217:
             return
 
@@ -264,7 +361,14 @@ class Editor(QPlainTextEdit):
             textCursor.select(QTextCursor.WordUnderCursor)
             if textCursor.selectedText == "":
                 return
+        if key == 16777219:
+            if self.textUnderCursor() in ['""', '()', '[]', "''", "{}"]:
+                textCursor = self.textCursor()
+                textCursorPos = textCursor.position()
 
+                textCursor.setPosition(textCursorPos + 1)
+                textCursor.deletePreviousChar()
+                self.setTextCursor(textCursor)
         if key not in [16777217, 16777219, 16777220]:
             super().keyPressEvent(e)
             return
