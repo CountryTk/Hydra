03163561b37e3bd6095b96ae28609c6f4fc684eb
Node <jordansavell1198@gmail.com>
2019-02-12 16:21:34 +0000

    Cleaned lastFileOpen.

--- src/utils/lastFileOpen.py
+++ src/utils/lastFileOpen.py
@@ -1,12 +1,10 @@
+import os
+
 
 def updateLastFileOpen(filepath):
-    """
-        Updates the path in the file that stores the last file edited with PyPad
-        If this config file can't be accessed for any reason it returns False
-    """
     try:
-        open(filepath, 'r').close()
-    except FileNotFoundError:
+        os.path.isfile(filepath)
+    except (FileNotFoundError, PermissionError, OSError):
         return False
 
     with open('resources/lastFile.txt', 'w') as f:
@@ -15,21 +13,12 @@ def updateLastFileOpen(filepath):
 
 
 def lastFileOpen():
-    """
-        Returns the path to the last file edited with PyPad
-        If this filepath can't be accessed for any reason it returns None
-    """
     try:
-        # Path to last file opened is kept in special config file
-        with open('resources/lastFile.txt', 'r') as f:
-            filepath = f.read().strip()
-
-        # Check file will open
-        open(filepath, 'r').close()
-
-        return filepath
-
-    # If ethier open fails return none
-    except FileNotFoundError:
+        file = open('resources/lastFile.txt', 'r+')
+        try:
+            return file.read().strip()
+        finally:
+            file.close()
+    except (FileNotFoundError, PermissionError, OSError):
         print('last-file-open config file not found')
         return None
