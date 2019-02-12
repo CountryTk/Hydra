b5a3810b9b793010fd073ba65b8ed84826c9edae
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2019-02-12 18:51:37 +0200

    Add new update checker

---
+++ src/utils/check_update.py
@@ -0,0 +1,51 @@
+import logging
+import requests
+
+
+def local_version():
+
+    logger = logging
+    logger.basicConfig(filename="pypad.log", filemode='a', level=logger.INFO)
+
+    file = "version.txt"
+
+    try:
+        with open(file, "r") as local:
+            ver = local.read()
+            return str(ver)
+
+    except FileNotFoundError as err:
+        logger.exception(err)
+        logger.log(logger.INFO, "Unable to find file {}".format(file))
+        logger.log(logger.INFO, "Starting the updater...")
+
+        return "0"
+
+
+def online_version():
+    url = 'https://raw.githubusercontent.com/Fuchsiaff/PyPad/master/src/version.txt'
+    req_get = requests.request('GET', url)
+    ver = req_get.text
+
+    return str(ver)
+
+
+def show_update():
+
+    local = local_version().strip()
+    online = online_version().strip()
+
+    if local != online:
+        return "An update is available, would you like to update?"
+    else:
+        return "PyPad is up to date"
+
+
+def make_decision(choice):
+    
+    if choice:
+        pass
+        # TODO: Update
+
+    else:
+        pass
