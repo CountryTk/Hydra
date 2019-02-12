6c0d640f65d6c2bf3041880ef4590e640d8c81aa
Node <jordansavell1198@gmail.com>
2019-02-12 11:26:45 +0000

    Tidy config.py
    Tidied up config.py a little - removed the if/elif block in config_choice

--- src/utils/config.py
+++ src/utils/config.py
@@ -10,42 +10,34 @@ def config_reader(theme):
 
     if theme == 0:
         try:
-            open(json_file_path.format(json_file[0]), "r")
+            open(json_file_path.format(json_file[0]), 'r')
         except FileNotFoundError as err:
             logger.exception(err)
-            logger.log(logger.INFO, "Attempting to correct directory and find config.json")
-            logger.log(logger.INFO, os.getcwd())
+            logger.info(os.getcwd() + ' Attempting to correct directory and find config.json')
             os.chdir(default_path)
             open(json_file_path.format(json_file[0]), 'r')
 
     elif theme == 1:
         try:
-            open(json_file_path.format(json_file[1]), "r")
+            open(json_file_path.format(json_file[1]), 'r')
         except FileNotFoundError as err:
             logger.exception(err)
-            logger.log(logger.INFO, "Unable to find theme with int 1")
+            logger.info('Unable to find theme with int 1')
     elif theme == 2:
         try:
-            open(json_file_path.format(json_file[2]), "r")
+            open(json_file_path.format(json_file[2]), 'r')
         except FileNotFoundError as err:
             logger.exception(err)
-            logger.log(logger.INFO, "Unable to find theme with int 2")
+            logger.info('Unable to find theme with int 2')
     else:
-        print("Error occurred")
-    text = open(json_file_path.format(json_file[theme]), "r").read()
+        print('Error occurred')
+    text = open(json_file_path.format(json_file[theme]), 'r').read()
     return json.loads(text)
 
 
 def config_choice(file):
     with open(file, 'r') as index_file:
         config_chosen = int(index_file.read())
-
-        if config_chosen == 0:
-            config = config_reader(0)
-        elif config_chosen == 1:
-            config = config_reader(1)
-        elif config_chosen == 2:
-            config = config_reader(2)
-        else:
-            config = config_reader(1)
+    while config_chosen:
+        config = config_reader(config_chosen)
         return config
