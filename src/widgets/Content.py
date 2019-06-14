8b6b2d7080c4b8b8e69a8d212fb57c246878d6af
CountryTk <31344416+CountryTk@users.noreply.github.com>
2019-06-14 12:56:20 +0300

    big update

--- src/widgets/Content.py
+++ src/widgets/Content.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QCompleter, QShortcut, QPlainTextEdit, QStatusBar, QLabel
 from PyQt5.QtGui import QFont, QKeySequence, QTextCursor
 from PyQt5.QtCore import Qt, QPoint
-from utils.predictionList import wordList
-from utils.config import config_reader
-from utils.search_algorithm import tokenize
-from widgets.TextEditor import Editor
+from src.utils.predictionList import wordList
+from src.utils.config import config_reader
+from src.utils.search_algorithm import tokenize
+from src.widgets.Editor import Editor
+from src.widgets.Pythonhighlighter import PyHighlighter
+from src.widgets.Completer import Completer
 
 config0 = config_reader(0)
 config1 = config_reader(1)
@@ -24,11 +26,11 @@ else:
 
 
 class Content(QWidget):
-    def __init__(self, text, fileName, baseName, parent, window):
+    def __init__(self, text, fileName, baseName, parent, window, isReadOnly=False):
         super().__init__()
-        self.editor = Editor(self)
+        self.editor = Editor(self, isReadOnly)
         self.text = text
-        self.window = window  # This is be the <Main> class
+        self.window = window  # This is the <Main> class
         self.parent = parent
         self.wordlist = wordList
         self.fileName = fileName
@@ -40,7 +42,7 @@ class Content(QWidget):
         self.tabSize = editor["TabWidth"]
         self.editor.textChanged.connect(self.changeSaved)
         self.saved = True
-        self.editor.setText(str(text))
+        self.editor.setPlainText(str(text))
         self.main_layout = QVBoxLayout(self)
         self.hbox = QHBoxLayout()
         self.status_bar_layout = QHBoxLayout()
@@ -55,23 +57,24 @@ class Content(QWidget):
         self.line = None
         self.column = None
 
-        self.editor.cursorPositionChanged.connect(self.change_col)
+        self.completer = Completer(self.wordlist)
+        self.setCompleter(self.completer)
 
         if self.baseName.endswith(".py"):
-            self.editor.python_highlighter()
-        elif self.baseName.endswith(".c") or self.baseName.endswith(".cpp") or self.baseName.endswith(".h"):
-            self.editor.c_highlighter()
-        elif self.baseName.endswith(".json"):
-            self.editor.json_highlighter()
+            self.highlighter = PyHighlighter(self.editor.document())
+        elif self.baseName.endswith(".doc"):
+            self.highlighter = PyHighlighter(self.editor.document())
         self.editor.cursorPositionChanged.connect(self.change_col)
 
-    def change_col(self, line, column):
-        self.line = line
-        self.column = column
-        self.status_bar.setText("Line: " + str(line) + " Column: " + str(column) + "           Total: " +
-            str(self.editor.lines() - 1) + " lines" + "     Size: " + str(self.get_size(self.editor.text())) + " KiB")
+    def change_col(self):
+        textCursor = self.editor.textCursor()
 
-        self.status_bar.setFont(QFont("Iosevka", 11))
+        line = textCursor.blockNumber() + 1
+        column = textCursor.positionInBlock()
+        self.status_bar.setText("Line: " + str(line) + " Column: " + str(column) + "           Total: " +
+            str(self.editor.totalLines() - 1) + " lines" + "     Size: " + str(self.get_size(self.editor.toPlainText())) + " KiB")
+
+        self.status_bar.setFont(QFont(editor["statusBarFont"], editor["statusBarFontSize"]))
 
     def get_size(self, input):
         return round(len(input.encode("utf-8"))/1000)
@@ -96,6 +99,85 @@ class Content(QWidget):
                 if j not in self.wordlist:
                     self.wordlist.append(j)
 
+    def setCompleter(self, completer):
+
+        self.completer.setWidget(self)
+
+        completer.setCompletionMode(QCompleter.PopupCompletion)
+        completer.setCaseSensitivity(Qt.CaseInsensitive)
+        self.completer = completer
+
+        self.completer.insertText.connect(self.insertCompletion)
+
+
+    def insertCompletion(self, completion):
+        textCursor = self.editor.textCursor()
+
+        extra = (len(completion) - len(self.completer.completionPrefix()))
+
+        textCursor.movePosition(QTextCursor.Left)
+
+
+        textCursor.movePosition(QTextCursor.EndOfWord)
+        textCursor.insertText(completion[-extra:])
+
+        if completion.endswith("()"):
+            cursorPos = textCursor.position()
+            textCursor.setPosition(cursorPos - 1)
+
+        self.editor.setTextCursor(textCursor)
+
+
+    def textUnderCursor(self):
+        textCursor = self.editor.textCursor()
+        textCursor.select(QTextCursor.WordUnderCursor)
+
+        return textCursor.selectedText()
+
+
+    def focusInEvent(self, event):
+        if self.completer:
+            self.completer.setWidget(self);
+            QPlainTextEdit.focusInEvent(self, event)
+
+    def checkIfCanComplete(self):
+
+        pass
+
+    def keyPressEvent(self, event):
+
+        if self.completer and self.completer.popup() and self.completer.popup().isVisible():
+            if event.key() in (Qt.Key_Enter, Qt.Key_Return, Qt.Key_Escape, Qt.Key_Tab, Qt.Key_Backtab):
+                event.ignore()
+                return
+
+        isShortcut = (event.modifiers() == Qt.ControlModifier and
+                      event.key() == Qt.Key_B)
+
+        if not self.completer or not isShortcut:
+            QPlainTextEdit.keyPressEvent(self.editor, event)
+
+        completionPrefix = self.textUnderCursor()
+
+        if not isShortcut:
+            if self.completer.popup():
+                self.completer.popup().hide()
+            return
+
+        self.completer.setCompletionPrefix(completionPrefix)
+
+        popup = self.completer.popup()
+
+        popup.setFont(self.font)
+        popup.setCurrentIndex(
+            self.completer.completionModel().index(0, 0))
+
+        cr = self.editor.cursorRect()
+        cr.translate(QPoint(10, 10))
+        cr.setWidth \
+            (self.completer.popup().sizeHintForColumn(0) + self.completer.popup().verticalScrollBar().sizeHint().width())
+        self.completer.complete(cr)
+
     def getTextCursor(self):
         textCursor = self.editor.textCursor()
         textCursorPos = textCursor.position()
@@ -104,7 +186,7 @@ class Content(QWidget):
 
     def changeSaved(self):
 
-        self.modified = self.editor.isModified()
+        self.modified = self.editor.is_modified()
 
         try:
             if self.modified:
